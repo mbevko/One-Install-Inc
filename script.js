b1954516a68abaa5a92500c0cf3dee2128fa673e
Michael Evko <mbevko@gmail.com>
2021-04-13 15:43:13 -0400

    scroll logo

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const nav = document.querySelector(".nav_two");
 
 
-function stickyNavigation() { 
+const stickyNavigation = () => { 
 const navTop = nav.offsetTop;
   
   if(window.scrollY > 0){
@@ -10,6 +10,14 @@ const navTop = nav.offsetTop;
     nav.style.transform = "translate3d(0, -105%, 0)";
   }
 }
+let scrollObject = document.querySelector(".scroll_logo");
+  
+  
 
 
-window.addEventListener('scroll', stickyNavigation);
+
+window.addEventListener('scroll', () => {
+  scrollObject.style.cssText = `transform: rotate(${window.pageYOffset}deg)`;
+  
+  stickyNavigation()
+});
