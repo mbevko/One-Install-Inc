f769f7724752118428075671a870a0b15ce92387
Michael Evko <mbevko@gmail.com>
2021-05-18 11:34:28 -0400

    contact transition

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ let scrollObject = document.querySelector(".scroll_logo");
 window.addEventListener('scroll', () => {
   stickyNavigation()
 });
+
+let left = document.querySelector(".left");
+let right = document.querySelector(".right");
+let hamburgerMenu = document.querySelector(".secondary_menu")
+
+window.addEventListener('scroll', () => {
+    
+    if(scrollY >= 125){
+        left.style.cssText = "transform: translateX(0); opacity: 1;"
+        right.style.cssText = "transform: translateX(0); opacity: 1;" 
+        hamburgerMenu.style.cssText = "opacity: 1; pointer-events: all"
+    } else if(scrollY < 125){
+        hamburgerMenu.style.cssText = "opacity: 0; pointer-events: none"
+    }
+    console.log(scrollY);
+})
