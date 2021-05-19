0bd08ee75ac3d2dae4ef61ddbfa9b146f0537cc6
Michael Evko <mbevko@gmail.com>
2021-05-19 18:11:21 -0400

    changes

--- script.js
+++ script.js
@@ -24,8 +24,8 @@ let hamburgerMenu = document.querySelector(".secondary_menu")
 window.addEventListener('scroll', () => {
     
     if(scrollY >= 125){
-        left.style.cssText = "transform: translateX(0); opacity: 1;"
-        right.style.cssText = "transform: translateX(0); opacity: 1;" 
+        //left.style.cssText = "transform: translateX(0); opacity: 1;"
+        //right.style.cssText = "transform: translateX(0); opacity: 1;" 
         hamburgerMenu.style.cssText = "opacity: 1; pointer-events: all"
     } else if(scrollY < 125){
         hamburgerMenu.style.cssText = "opacity: 0; pointer-events: none"
