b9521fe2e78ced0db5a27d7a6e3113adc455ecae
Michael Evko <mbevko@gmail.com>
2021-05-20 09:28:03 -0400

    contact animation

--- script.js
+++ script.js
@@ -21,11 +21,16 @@ let left = document.querySelector(".left");
 let right = document.querySelector(".right");
 let hamburgerMenu = document.querySelector(".secondary_menu")
 
+const contactAnimation = () => {
+  left.style.cssText = "transform: translateX(0); opacity: 1;"
+  right.style.cssText = "transform: translateX(0); opacity: 1;" 
+}
+
+window.onload(contactAnimation())
+
 window.addEventListener('scroll', () => {
     
     if(scrollY >= 125){
-        //left.style.cssText = "transform: translateX(0); opacity: 1;"
-        //right.style.cssText = "transform: translateX(0); opacity: 1;" 
         hamburgerMenu.style.cssText = "opacity: 1; pointer-events: all"
     } else if(scrollY < 125){
         hamburgerMenu.style.cssText = "opacity: 0; pointer-events: none"
